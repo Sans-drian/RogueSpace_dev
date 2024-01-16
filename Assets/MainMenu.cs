74edb2a244982affcf19f7179df77bc567a3c1dc
Sulthan Syuza Prabowo <sulthanprabowo7@gmail.com>
2024-01-16 11:22:21 +0700

    Made simple main menu, and multiplayer menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// Initiate unity scenemanagement
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // public void singleplayer
+    public void SinglePlayer()
+    {
+        // Load scene 2
+        SceneManager.LoadScene(2);
+    }
+
+    // public void multiplayer
+    public void MultiPlayer()
+    {
+        // Load scene 1
+        SceneManager.LoadScene(1);
+    }
+
+    // public void quit
+    public void Quit()
+    {
+        // Quit game
+        Application.Quit();
+    }
+
+}
